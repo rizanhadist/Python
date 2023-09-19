29bc61e7171932d532df468d26e8f41cd68aa360
rizanhadist <rizansmage@proton.me>
2023-09-19 20:25:10 +0700

    last session

--- tipe_data.py
+++ tipe_data.py
@@ -2,3 +2,41 @@ data = "joko"
 data1 = "pintar"
 data2 = str(100)
 print (data + data1 + data2)
+
+
+#snakecase
+data_list = ["Fajri, 28, 3000.0, True"]
+print(data_list)
+data_list[0] = "New Fajri"
+print(data_list)
+#tuple
+sample_tuple = ("fajri", 28, False)
+
+set 
+data = {"Banana", "Apple", "Grap"}
+data.add("koko")
+print(data)
+
+
+Dictionary
+
+
+mobil = {
+    "name" : "BMW",
+    "maxSpeed" : 300.00,
+    "isExpensive" : True,
+    "color" : ["red", "blue", "green"]
+}
+print(mobil["maxSpeed"])
+
+
+
+data = {"banana", "apple", "grape", "apple", "apple"}
+data.add("cherry")
+data.remove("banana")
+print(data)
+
+
+dart = 3.111
+
+#Oeprator
