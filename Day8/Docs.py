cc32f423121168b8f42bdc111fd5d8790aa7aa3e
rizanhadist <rizansmage@proton.me>
2023-10-09 20:55:59 +0700

    update file name & add new refactor

---
+++ Day8/Docs.py
@@ -0,0 +1,18 @@
+#komentar for sharks whicj variabel of list of string
+sharks = ['hammerhead', 'great white', 'dogfish', 'frilled']
+
+print(sharks)
+
+
+# Hello
+# this is my code
+# which can do anything
+#Especially
+# callculation stuff
+# fell free to use
+
+""""
+jsjhkdkjasd
+askdjsadkljyuiod
+klsjdkl;j
+"""
